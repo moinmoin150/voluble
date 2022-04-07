a89a6a771f325016474ef02f7f8f09302f82ce72
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-04-07 17:16:24 -0500

    Update download_bw.py

--- download_bw.py
+++ download_bw.py
@@ -112,7 +112,7 @@ if st.session_state['button'] == True:
                     out.append(tweet._json)
         st.write(f"Complete! Total Number: {len(out)}")
 	st.session_state['button'] = False
-
+	
 col_list = ['created_at', 'id', 'id_str', 'full_text', 'source', 'in_reply_to_status_id',
 'in_reply_to_status_id_str', 'in_reply_to_user_id', 'in_reply_to_user_id_str', 'in_reply_to_screen_name',
 'user', 'retweeted_status', 'retweet_count', 'favorite_count', 'lang']
