a356c104eade07afeef5b9b85c4bcd8f1000cbec
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-04-07 15:50:10 -0500

    Add files via upload

---
+++ download_bw.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import pandas as pd
+import streamlit.components as stc
+import base64
+import time
+import datetime
+import requests
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+class FileDownloader(object):
+
+	def __init__(self, data,filename='myfile',file_ext='txt'):
+		super(FileDownloader, self).__init__()
+		self.data = data
+		self.filename = filename
+		self.file_ext = file_ext
+
+	def download_id(self):
+		b64 = base64.b64encode(self.data.encode()).decode()
+		new_filename = "{}_{}_.{}".format(self.filename,timestr,self.file_ext)
+		st.markdown("#### Download ID list ###")
+		href = f'<a href="data:file/{self.file_ext};base64,{b64}" download="{new_filename}">Click Here</a>'
+		st.markdown(href,unsafe_allow_html=True)
+
+# request access token
+params = {
+"password":st.secrets["password"]
+}
+url = 'https://api.brandwatch.com/oauth/token?username=tzhu@gbexpertsgroup.com&grant_type=api-password&client_id=brandwatch-api-client'
+r = requests.get(url, params=params).json()
+access_token = r['access_token']
+h = {
+"Authorization":f"Bearer {access_token}"
+}
+
+# request query list
+url = 'https://api.brandwatch.com/projects/1998290339/queries/summary'
+r = requests.get(url, headers=h).json()
+queries = [i['name'] for i in r['results']]
+
+# user input query name
+st.markdown("# Download Tweets From Brandwatch")
+query = st.selectbox("Select the query", queries)
+_id = [i['id'] for i in r['results'] if i['name']==query][0]
+
+# user select date range
+start_date = st.date_input("Select start date")
+end_date = st.date_input("Select end date")
+start = start_date.strftime("%Y-%m-%d")
+end = end_date.strftime("%Y-%m-%d")
+
+# preview the number of data points to download
+url = f"https://api.brandwatch.com/projects/1998290339/data/mentions/count?queryId%5B%5D={_id}&startDate={start}&endDate={end}"
+r = requests.get(url, headers=h).json()
+st.write(f"Ready to download {r['mentionsCount']} data points")
+
+# start downloading
+url = f"https://api.brandwatch.com/projects/1998290339/data/mentions?queryId={_id}&startDate={start}&endDate={end}&pageSize=5000&orderBy=date&orderDirection=asc"
+r = requests.get(url, headers=h).json()
+ids = [i['guid'] for i in r['results']]
+st.write('Downloading in process...')
+while 'nextCursor' in r:
+    cursor = r['nextCursor']
+    url = f"https://api.brandwatch.com/projects/1998290339/data/mentions?queryId={_id}&startDate={start}&endDate={end}&pageSize=5000&orderBy=date&orderDirection=asc&cursor={cursor}"
+    r = requests.get(url, headers=h).json()
+    ids += [i['guid'] for i in r['results']]
+    st.write(f"Downloaded {len(ids} IDs")
+
+df = pd.DataFrame({
+'ID':ids
+})
+st.dataframe(df)
+download = FileDownloader(df.to_csv(),file_ext='csv').download_id()
